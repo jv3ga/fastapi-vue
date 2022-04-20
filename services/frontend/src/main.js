faf55e07d06eb45aa0deba40889e659ef97240d9
Joel Vega <joel.v3ga@gmail.com>
2022-04-20 19:31:29 +0200

    feat: install vuetify

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -5,7 +5,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
+Vue.use(Vuetify)
+
+const opts = {}
+
+export default new Vuetify(opts)
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:5000/';  // the FastAPI backend
